12d0532585b24711f6c1deb00e68b35abf9a702c
nguyentien00 <149949507+nguyentien00@users.noreply.github.com>
2024-07-22 14:40:57 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+	/*
+					INFO
+		------------------------------------
+		Tên: Tiendev
+		Sinh năm: 2K5
+		FB: https://facebook.com
+
+		* Vui lòng không xóa dòng nầy để tôn trọng tác giả!
+	*/
+
+	//gọi route
+	require_once __DIR__."/app/include/core/route.php";
+?>
